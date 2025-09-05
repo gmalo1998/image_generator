c4a18b6784d1f66506fb96ad0dfaac338cc7b436
Ganesh Malo <malo.ganesh98@gmail.com>
2025-09-06 00:54:31 +0530

    Updates

---
+++ assets/Navbar-BwrygeIx.js
@@ -0,0 +1,11 @@
+import{j as s}from"./index-BaNwPpuI.js";import{c}from"./createLucideIcon-oiRCnwst.js";import{D as d}from"./download-CVaK9Xlc.js";/**
+ * @license lucide-react v0.541.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]],h=c("chevron-down",i);/**
+ * @license lucide-react v0.541.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=[["rect",{width:"7",height:"7",x:"3",y:"3",rx:"1",key:"1g98yp"}],["rect",{width:"7",height:"7",x:"14",y:"3",rx:"1",key:"6d4xhi"}],["rect",{width:"7",height:"7",x:"14",y:"14",rx:"1",key:"nxv5o0"}],["rect",{width:"7",height:"7",x:"3",y:"14",rx:"1",key:"1bb6yr"}]],m=c("layout-grid",x),v=({setFormat:l,setOpen:a,open:o,format:n,handleDownload:r,setOpenPopup:t})=>s.jsxs("nav",{className:"navbar",children:[s.jsxs("div",{className:"navbar-logo",children:[s.jsx("div",{className:"navbar-logo-circle",children:s.jsx("span",{className:"navbar-logo-text",children:"WC"})}),s.jsx("h1",{className:"navbar-title",children:"WordCanvas"})]}),s.jsxs("div",{className:"navbar-actions",children:[s.jsx("div",{className:"templatebutton",children:s.jsxs("button",{onClick:()=>t(!0),className:"choose-template-btn",children:[s.jsx(m,{className:"grid"}),s.jsx("p",{children:"Choose Template"})]})}),s.jsxs("div",{className:"dropdown",children:[s.jsxs("button",{className:"dropdown-btn",onClick:()=>{a(!o)},children:[n,s.jsx(h,{className:"icon"})]}),o&&s.jsx("div",{className:"dropdown-menu",children:s.jsx("ul",{children:["toPNG","toJPEG","toSVG"].map(e=>s.jsx("li",{children:s.jsx("button",{className:"dropdown-item",onClick:()=>{l(e),a(!1)},children:e})},e))})})]}),s.jsxs("button",{onClick:r,disabled:n==="Select Format",className:"download-btn-one",children:[s.jsx(d,{className:"download-icon1"})," ",s.jsx("p",{className:"download-text",children:"Download"})]})]})]});export{v as default};
